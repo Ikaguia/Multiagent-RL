5966f88351154868ea2756abc3a48645496cb404
Ikaguia <cristianobrust123@gmail.com>
2018-08-15 00:11:36 -0300

    process_results

--- pacman/results/process_results.py
+++ pacman/results/process_results.py
@@ -4,14 +4,15 @@ import os
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
 import plot
 
+import numpy as np
+import matplotlib.pyplot as plt
 
-files  = [file for file in glob.glob('*.txt') + glob.glob('results/*.txt') if file.split('/')[-1].split('-')[0] in ('learn','test')]
 
-learnRes = {}
+files  = [file for file in glob.glob('*.txt') + glob.glob('results/*.txt') if file.split('/')[-1].split('-')[0] in ('test',)]
+
 testRes = {}
 
 for fileName in sorted(files):
-	print fileName
 	try:
 		with open(fileName, 'r') as file:
 			lines = [line for line in file]
@@ -27,24 +28,38 @@ for fileName in sorted(files):
 		for line in lines[testStart+1:]:
 			if not line.startswith('aF'): break
 			testScores += [float(line[2:])]
-		# if fileName == 'learn-ai2_2g_fleet_100.txt':
-		# 	print learnScores
-		# 	print testScores
-		if fileName.split('-')[0] == 'learn':
-			learnRes[tuple(fileName.split('-')[1].split('_'))] = (
-				plot.calculate_regression_coefficients(learnScores, degree=1)[1],
-				plot.calculate_regression_coefficients(learnScores, degree=1)[0],
-			)
 		if fileName.split('-')[0] == 'test':
 			testRes[tuple(fileName.split('-')[1].split('_'))] = (
 				sum(testScores) / len(testScores),
 			)
 	except:
-		print "failed"
+		print fileName,"failed"
 
-print "learn results:"
-for result in sorted(learnRes):
-	print "\t",result,": %d + %fx" % learnRes[result]
-print "test results:"
-for result in sorted(testRes):
-	print "\t",result,":",testRes[result][0]
+
+agents = sorted(set([i[0] for i in testRes]))
+nghosts = sorted(set([i[1] for i in testRes]))
+colors = 'bgrcmykw'
+
+for nghost in nghosts:
+	for agentId in xrange(len(agents)):
+		agent = agents[agentId]
+		color = colors[agentId]
+		x,y = [],[]
+		for result in sorted(testRes):
+			if result[:2] == (agent,nghost):
+				x += [float(result[2])]
+				y += [testRes[result][0]]
+		z = np.polyfit(x, y, len(x)-1)
+		# print x,y,z
+		p = np.poly1d(z)
+		s = np.linspace(0, x[-1] + 50, 100)
+		plt.plot(x, y, color+'.')
+		plt.plot(s, p(s), color+'--', label=agent)
+
+		for lgames in (100, 150, 200, 250, 500, 1000):
+			print "estimated score for %s %s after %d learn games: %f" % (agent, nghost, lgames, p(lgames))
+	plt.xlabel('learn games')
+	plt.ylabel('average score in test games')
+	plt.legend()
+	plt.show()
+
